b5706ac1291c1b66c8b9358f42a4c15bd1164a75
Simone Parmeggiani <parmeggiani.simone@gmail.com>
2024-12-14 10:25:05 +0100

    cambiato formattazione countdown

--- vtesitaly/lib/views/rows/event.dart
+++ vtesitaly/lib/views/rows/event.dart
@@ -15,6 +15,8 @@ class _EventRowState extends State<EventRow> {
   late Timer _timer;
   Duration _remainingTime = const Duration();
   final DateTime _targetDateTime = DateTime(2025, 3, 1, 9, 30, 0);
+  String formattedCountdown = "";
+
 
   @override
   void initState() {
@@ -23,7 +25,8 @@ class _EventRowState extends State<EventRow> {
   }
 
 
-  @override void dispose() {
+  @override 
+  void dispose() {
     _timer.cancel();
     super.dispose();
   }
@@ -41,11 +44,31 @@ class _EventRowState extends State<EventRow> {
     final DateTime now = DateTime.now();
     setState(() {
       _remainingTime = _targetDateTime.difference(now);
+
       if (_remainingTime.isNegative) {
         _timer.cancel();
+        formattedCountdown = "Evento Iniziato!";
+      } else {
+        final int totalDays = _remainingTime.inDays;
+        final int years = totalDays ~/ 365;
+        final int months = (totalDays % 365) ~/ 30;
+        final int days = (totalDays % 365) % 30;
+        final int hours = _remainingTime.inHours % 24;
+        final int minutes = _remainingTime.inMinutes % 60;
+        final int seconds = _remainingTime.inSeconds % 60;
+
+        
+        formattedCountdown = '${years > 0 ? "$years anni " : ""}'
+            '${months > 0 ? "$months months " : ""}'
+            '${days > 0 ? "$days days " : ""}'
+            '${hours.toString().padLeft(2, '0')}:'
+            '${minutes.toString().padLeft(2, '0')}:'
+            '${seconds.toString().padLeft(2, '0')}';
       }
     });
   }
+
+
 
 
   @override
@@ -59,17 +82,7 @@ class _EventRowState extends State<EventRow> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
-              "Italian Grand Prix Modena", 
-              style: TextStyle(
-                fontSize: 25, 
-                fontWeight: FontWeight.bold
-              )
-            ),
-            const SizedBox(
-              height: 16
-            ),
-            const Text(
-              "2025", 
+              "Italian Grand Prix 2024-25", 
               style: TextStyle(
                 fontSize: 40, 
                 fontWeight: FontWeight.bold
@@ -83,14 +96,14 @@ class _EventRowState extends State<EventRow> {
               style: TextStyle(
                 fontSize: 18, 
                 fontWeight: FontWeight.w300,
-                color: Colors.grey
+                color: Colors.blue
               )
             ),
             const SizedBox(
               height: 4
             ),
             const Text(
-              "Modena, Italy", 
+              "March 1st, 2025", 
               style: TextStyle(
                 fontSize: 18, 
                 fontWeight: FontWeight.w300,
@@ -101,7 +114,7 @@ class _EventRowState extends State<EventRow> {
               height: 12
             ),
             const Text(
-              "Countdown", 
+              "Event starts in:", 
               style: TextStyle(
                 fontSize: 18, 
                 fontWeight: FontWeight.w300,
@@ -109,12 +122,12 @@ class _EventRowState extends State<EventRow> {
               )
             ),
             const SizedBox(
-              height: 12
+              height: 20
             ),
             Text(
-              _remainingTime.toString(), 
+              formattedCountdown, 
               style: const TextStyle(
-                fontSize: 18, 
+                fontSize: 25, 
                 fontWeight: FontWeight.w300,
                 color: Colors.black
               )
@@ -127,15 +140,16 @@ class _EventRowState extends State<EventRow> {
               style: TextStyle(
                 fontSize: 30, 
                 fontWeight: FontWeight.bold,
-                color: Colors.black
+                color: Colors.red
               )
             ),
           ],
         ),
         Image.asset(
-          "assets/images/vtes_italy.jpeg",
+          "assets/images/KarlSchrekt.jpeg",
           // width: MediaQuery.of(context).size.width/2-32,
           width: min(500, MediaQuery.of(context).size.width/2-32),
+          height: min(500, MediaQuery.of(context).size.width/2-32),
           fit: BoxFit.cover,
         )
       ],
