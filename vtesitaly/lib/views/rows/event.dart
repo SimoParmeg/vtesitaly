37f24c67df285a10b0c6063b92d670659c699c63
Simone Parmeggiani <parmeggiani.simone@gmail.com>
2024-12-30 17:21:40 +0100

    aggiunto popup con form

--- vtesitaly/lib/views/rows/event.dart
+++ vtesitaly/lib/views/rows/event.dart
@@ -1,11 +1,7 @@
-import 'dart:async';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:vtesitaly/config.dart';
+import 'package:vtesitaly/views/forms/registration.dart';
 
 class EventRow extends StatefulWidget {
-
   const EventRow({super.key});
 
   @override
@@ -13,90 +9,53 @@ class EventRow extends StatefulWidget {
 }
 
 class _EventRowState extends State<EventRow> {
-
-  late Timer _timer;
-  Duration _remainingTime = const Duration();
-  final DateTime _targetDateTime = DateTime(2025, 3, 1, 9, 30, 0);
-  String formattedCountdown = "";
-
-
-  @override
-  void initState() {
-    super.initState();
-    _startCountdown();
+  void _showSubscriptionDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: const SubscriptionForm(), // Mostra il modulo
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
-
-
-  @override 
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-
-  void _startCountdown() {
-    _updateRemainingTime();
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      _updateRemainingTime();
-    });
-  }
-
-
-  void _updateRemainingTime() {
-    final DateTime now = DateTime.now();
-    setState(() {
-      _remainingTime = _targetDateTime.difference(now);
-
-      if (_remainingTime.isNegative) {
-        _timer.cancel();
-        formattedCountdown = "Event Started!";
-      } else {
-        final int totalDays = _remainingTime.inDays;
-        final int years = totalDays ~/ 365;
-        final int months = (totalDays % 365) ~/ 30;
-        final int days = (totalDays % 365) % 30;
-        final int hours = _remainingTime.inHours % 24;
-        final int minutes = _remainingTime.inMinutes % 60;
-        final int seconds = _remainingTime.inSeconds % 60;
-
-        formattedCountdown = '${years > 0 ? "$years anni " : ""}'
-            '${months > 0 ? "$months months " : ""}'
-            '${days > 0 ? "$days days " : ""}'
-            '${hours.toString().padLeft(2, '0')}:'
-            '${minutes.toString().padLeft(2, '0')}:'
-            '${seconds.toString().padLeft(2, '0')}';
-      }
-    });
-  }
-
 
   @override
   Widget build(BuildContext context) {
-    final isMobile = MediaQuery.of(context).size.width < TRESHOLD_MOBILEMAXWIDTH;
-    
-    return !isMobile ? Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _buildColumnWidget(),
-        _buildImageWidget(isMobile),
-        
-      ]
-    ) : Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _buildImageWidget(isMobile),
-        const SizedBox(height:20),
-        _buildColumnWidget()        
-      ]
-    );
-
+    final isMobile = MediaQuery.of(context).size.width < 600; // Definisci la tua soglia
+    return !isMobile
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildColumnWidget(context),
+              _buildImageWidget(isMobile),
+            ],
+          )
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildImageWidget(isMobile),
+              const SizedBox(height: 20),
+              _buildColumnWidget(context),
+            ],
+          );
   }
 
-  Widget _buildColumnWidget(){
+  Widget _buildColumnWidget(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         RichText(
@@ -105,7 +64,7 @@ class _EventRowState extends State<EventRow> {
               TextSpan(
                 text: "Italian ",
                 style: TextStyle(
-                  fontSize: 48, 
+                  fontSize: 48,
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
                 ),
@@ -113,94 +72,87 @@ class _EventRowState extends State<EventRow> {
               TextSpan(
                 text: "Grand Prix ",
                 style: TextStyle(
-                  fontSize: 48, 
+                  fontSize: 48,
                   fontWeight: FontWeight.bold,
-                  color: Colors.blue ,
+                  color: Colors.blue,
                 ),
               ),
               TextSpan(
                 text: "2024-25",
                 style: TextStyle(
-                  fontSize: 48, 
+                  fontSize: 48,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black, 
+                  color: Colors.black,
                 ),
               ),
             ],
           ),
         ),
-        const SizedBox(
-          height: 16
-        ),
+        const SizedBox(height: 16),
         Text(
-          "Modena, Italy", 
+          "Modena, Italy",
           style: TextStyle(
-            fontSize: 20, 
+            fontSize: 20,
             fontWeight: FontWeight.w900,
-            color: Colors.black.withOpacity(0.6)
-          )
+            color: Colors.black.withOpacity(0.6),
+          ),
         ),
-        const SizedBox(
-          height: 4
-        ),
+        const SizedBox(height: 4),
         const Text(
-          "March 1st, 2025", 
+          "March 1st, 2025",
           style: TextStyle(
-            fontSize: 20, 
+            fontSize: 20,
             fontWeight: FontWeight.w900,
-            color: Colors.blue
-          )
+            color: Colors.blue,
+          ),
         ),
-        const SizedBox(
-          height: 12
-        ),
+        const SizedBox(height: 12),
         const Text(
-          "Event starts in:", 
+          "Event starts in:",
           style: TextStyle(
-            fontSize: 20, 
+            fontSize: 20,
             fontWeight: FontWeight.w900,
-            color: Colors.black
-          )
+            color: Colors.black,
+          ),
         ),
-        const SizedBox(
-          height: 20
-        ),
-        Text(
-          formattedCountdown, 
-          style: const TextStyle(
-            fontSize: 25, 
-            fontWeight: FontWeight.w800,
-            color: Colors.black
-          )
-        ),
-        const SizedBox(
-          height: 12
-        ),
+        const SizedBox(height: 20),
         const Text(
-          "Subscriptions Available Soon!", 
+          "Subscriptions Available Soon!",
           style: TextStyle(
-            fontSize: 30, 
+            fontSize: 30,
             fontWeight: FontWeight.bold,
-            color: Colors.red
-          )
+            color: Colors.red,
+          ),
+        ),
+        GestureDetector(
+          onTap: () => _showSubscriptionDialog(context), // Apre il popup
+          child: const Text(
+            "Subscribe Here!",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.red,
+              decoration: TextDecoration.underline,
+            ),
+          ),
         ),
       ],
     );
   }
 
-  Widget _buildImageWidget(bool isMobile){
+  Widget _buildImageWidget(bool isMobile) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(8),
       child: Image.asset(
         "assets/images/logo_gp.jpeg",
-        width: !isMobile 
-          ? min(475, MediaQuery.of(context).size.width/2-32) 
-          : MediaQuery.of(context).size.width-32,
-        height: !isMobile 
-          ? min(475, MediaQuery.of(context).size.width/2-32) 
-          : MediaQuery.of(context).size.width-32,
+        width: !isMobile
+            ? MediaQuery.of(context).size.width / 2 - 32
+            : MediaQuery.of(context).size.width - 32,
+        height: !isMobile
+            ? MediaQuery.of(context).size.width / 2 - 32
+            : MediaQuery.of(context).size.width - 32,
         fit: BoxFit.cover,
-      )
+      ),
     );
   }
 }
