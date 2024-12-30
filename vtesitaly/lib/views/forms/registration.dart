f9cfd7d91e6a8d271e0f10ae9a6d2bc104d2bc25
Simone Parmeggiani <parmeggiani.simone@gmail.com>
2024-12-30 18:37:29 +0100

    first draft

---
+++ vtesitaly/lib/views/forms/registration.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class SubscriptionForm extends StatefulWidget {
+  const SubscriptionForm({super.key});
+
+  @override
+  State<SubscriptionForm> createState() => _SubscriptionFormState();
+}
+
+class _SubscriptionFormState extends State<SubscriptionForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _surnameController = TextEditingController();
+  final TextEditingController _idVeknController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _decklistController = TextEditingController();
+
+  final FocusNode _nameFocus = FocusNode();
+  final FocusNode _surnameFocus = FocusNode();
+  final FocusNode _idVeknFocus = FocusNode();
+  final FocusNode _emailFocus = FocusNode();
+  final FocusNode _decklistFocus = FocusNode();
+
+  int _subscriptionType = 0; // Default: not selected
+
+  @override
+  void dispose() {
+    // Dispose controllers and focus nodes
+    _nameController.dispose();
+    _surnameController.dispose();
+    _idVeknController.dispose();
+    _emailController.dispose();
+    _decklistController.dispose();
+
+    _nameFocus.dispose();
+    _surnameFocus.dispose();
+    _idVeknFocus.dispose();
+    _emailFocus.dispose();
+    _decklistFocus.dispose();
+
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      final data = {
+        "name": _nameController.text,
+        "surname": _surnameController.text,
+        "id_vekn": _idVeknController.text,
+        "email": _emailController.text,
+        "decklist": _decklistController.text,
+        "subscription_type": _subscriptionType,
+      };
+
+      _sendDataToBackend(data);
+    }
+  }
+
+  void _sendDataToBackend(Map<String, dynamic> data) async {
+    const backendUrl = "https://vtesitaly.com/api/register";
+
+    try {
+      final response = await http.post(
+        Uri.parse(backendUrl),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode == 200) {
+        print("Registration completed!");
+      } else {
+        print("Server Error: ${response.body}");
+      }
+    } catch (e) {
+      print("Error during connection: $e");
+    }
+  }
+
+
+  void _cancelForm() {
+    // Clear all fields and return to the previous screen
+    _nameController.clear();
+    _surnameController.clear();
+    _idVeknController.clear();
+    _emailController.clear();
+    _decklistController.clear();
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            controller: _nameController,
+            focusNode: _nameFocus,
+            decoration: const InputDecoration(labelText: 'Name *'),
+            validator: (value) => value!.isEmpty ? 'Insert name' : null,
+            onFieldSubmitted: (_) {
+              FocusScope.of(context).requestFocus(_surnameFocus);
+            },
+          ),
+          TextFormField(
+            controller: _surnameController,
+            focusNode: _surnameFocus,
+            decoration: const InputDecoration(labelText: 'Surname *'),
+            validator: (value) => value!.isEmpty ? 'Insert surname' : null,
+            onFieldSubmitted: (_) {
+              FocusScope.of(context).requestFocus(_idVeknFocus);
+            },
+          ),
+          TextFormField(
+            controller: _idVeknController,
+            focusNode: _idVeknFocus,
+            decoration: const InputDecoration(labelText: 'VEKN ID *'),
+            keyboardType: TextInputType.number,
+            validator: (value) => value!.isEmpty ? 'Insert your VEKN ID' : null,
+            onFieldSubmitted: (_) {
+              FocusScope.of(context).requestFocus(_emailFocus);
+            },
+          ),
+          TextFormField(
+            controller: _emailController,
+            focusNode: _emailFocus,
+            decoration: const InputDecoration(labelText: 'Email *'),
+            keyboardType: TextInputType.emailAddress,
+            validator: (value) =>
+                value!.isEmpty ? 'Insert a valid mail' : null,
+            onFieldSubmitted: (_) {
+              FocusScope.of(context).requestFocus(_decklistFocus);
+            },
+          ),
+          TextFormField(
+            controller: _decklistController,
+            focusNode: _decklistFocus,
+            decoration: const InputDecoration(labelText: 'Decklist (optional, you can upload later)'),
+            maxLines: 4,
+          ),
+          DropdownButtonFormField<int>(
+            value: _subscriptionType,
+            items: const [
+              DropdownMenuItem(value: 0, child: Text("Select Subscription *")),
+              DropdownMenuItem(value: 1, child: Text("Only GP (Saturday), 60€")),
+              DropdownMenuItem(value: 2, child: Text("GP + Redemption Event (Saturday + Sunday), 95€")),
+            ],
+            onChanged: (value) {
+              setState(() {
+                _subscriptionType = value!;
+              });
+            },
+            decoration: const InputDecoration(
+              labelText: "",
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) =>
+              value == 0 ? 'Please select a subscription type' : null,
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: const Text("Register"),
+              ),
+              const SizedBox(width: 32),
+              OutlinedButton(
+                onPressed: _cancelForm,
+                child: const Text("Cancel"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
