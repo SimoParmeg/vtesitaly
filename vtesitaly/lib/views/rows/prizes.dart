6b627ee1a033e48827aef36e130c3defc4cae82f
Simone Parmeggiani <parmeggiani.simone@gmail.com>
2025-01-18 10:07:03 +0100

    slideshow prizes

--- vtesitaly/lib/views/rows/prizes.dart
+++ vtesitaly/lib/views/rows/prizes.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart'; // Import carousel_slider package
 import 'package:vtesitaly/config.dart';
 import 'package:vtesitaly/views/components/prizes_tile.dart';
 import 'package:vtesitaly/views/components/section_title.dart';
 
 class PrizesRow extends StatefulWidget {
-
   const PrizesRow({super.key});
 
   @override
@@ -12,29 +12,22 @@ class PrizesRow extends StatefulWidget {
 }
 
 class _PrizesRowState extends State<PrizesRow> {
-
   @override
   Widget build(BuildContext context) {
     final isMobile = MediaQuery.of(context).size.width < TRESHOLD_MOBILEMAXWIDTH;
-    
-    return !isMobile ? Column(
-      children: [
-        _buildTitleWidget(),
-        const SizedBox(height:20),
-        _buildSectionWidget(),
-        const SizedBox(height:20),
-        _buildColumnWidget(isMobile)
-      ],
-    ) : Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _buildTitleWidget(),
-        const SizedBox(height:20),
-        _buildSectionWidget(),
-        const SizedBox(height:20),
-        _buildColumnWidget(isMobile),
-      ]
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          _buildTitleWidget(),
+          const SizedBox(height: 20),
+          _buildSectionWidget(),
+          const SizedBox(height: 20),
+          _buildColumnWidget(isMobile),
+          const SizedBox(height: 20),
+          _buildCarouselSlider(), // Add the carousel slider here
+        ],
+      ),
     );
   }
 
@@ -48,23 +41,25 @@ class _PrizesRowState extends State<PrizesRow> {
     return SizedBox(
       height: !isMobile ? 420 : 840,
       width: MediaQuery.of(context).size.width - 64,
-      child: !isMobile ? Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: _buildItemsWidgets()
-      ) : Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: _buildItemsWidgets()
-      )
+      child: !isMobile
+          ? Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: _buildItemsWidgets(),
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: _buildItemsWidgets(),
+            ),
     );
   }
 
   Widget _buildSectionWidget() {
     return const Text(
-      "Winner prizes will be distributed in a pyramid manner based on the number of participants. Everyone will receive the welcome pack as door prize."
+      "Winner prizes will be distributed in a pyramid manner based on the number of participants. Everyone will receive the welcome pack as door prize.",
     );
   }
 
@@ -76,8 +71,6 @@ class _PrizesRowState extends State<PrizesRow> {
           const MapEntry("Grand Prix circuit playmat", Icons.tab_rounded),
           const MapEntry("Grand Prix circuit promo card", Icons.calendar_view_month_outlined),
           const MapEntry("Other gifts TBA soon", Icons.calendar_view_month_outlined),
-          // const MapEntry("Pack of cards (see image below)", Icons.calendar_view_month_outlined),
-          // const MapEntry("Edge sponsored by VTES Decks", Icons.refresh),
         ]),
       ),
       const SizedBox(height: 20),
@@ -86,11 +79,41 @@ class _PrizesRowState extends State<PrizesRow> {
         lines: Map.fromEntries([
           const MapEntry("Original 'One With the Land' Art, realized by Riccardo Fabiani", Icons.filter_1_rounded),
           const MapEntry("Commemorative Metal Rings for top 10 players", Icons.panorama_horizontal_select),
-          // const MapEntry("Counters by VTES Decks for the finalists", Icons.filter_1_rounded),
           const MapEntry("Sponsorized UltraPro material", Icons.filter_none_rounded),
           const MapEntry("Special prize TBA for the third round winners", Icons.calendar_view_month_outlined),
         ]),
       ),
     ];
+  }
+
+  Widget _buildCarouselSlider() {
+    final List<String> imagePaths = [
+      "assets/images/prizes/prize1.jpg",
+      "assets/images/prizes/prize2.jpg",
+      "assets/images/prizes/prize3.jpg",
+    ];
+
+    return CarouselSlider(
+      items: imagePaths.map((imagePath) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.asset(
+            imagePath,
+            width: MediaQuery.of(context).size.width - 32,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+        );
+      }).toList(),
+      options: CarouselOptions(
+        height: 300,
+        enlargeCenterPage: true,
+        autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 3),
+        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+        aspectRatio: 16 / 9,
+        viewportFraction: 0.8,
+      ),
+    );
   }
 }
